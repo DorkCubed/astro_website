278b6c8ff9dc1eca2119ee8c511f47b55b04b5b5
DorkCubed <f20231087@pilani.bits-pilani.ac.in>
2024-07-01 03:03:12 +0530

    popups added
    Signed-off-by: DorkCubed <f20231087@pilani.bits-pilani.ac.in>

--- src/components/about-us/Picture.tsx
+++ src/components/about-us/Picture.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { useFloating, useHover, useInteractions, safePolygon, autoUpdate, FloatingArrow, arrow, offset, autoPlacement } from '@floating-ui/react';
 
 interface PictureProps {
     src: string;
@@ -11,17 +12,43 @@ interface PictureProps {
 function Picture(props: PictureProps) {
     let [isHovered, setIsHovered] = useState(false);
 
+    const arrowRef = useRef(null);
+    const { refs, floatingStyles, context } = useFloating({
+        whileElementsMounted: autoUpdate,
+        middleware: [arrow({ element: arrowRef }), offset(10), autoPlacement({ allowedPlacements: ['top', 'bottom'] })],
+        open: isHovered,
+        onOpenChange: setIsHovered,
+    });
+
+    const hover = useHover(context, { handleClose: safePolygon() });
+    const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
+
     return (
-        <div className="potrait-images-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-            <img src={props.src} alt={props.alt} onClick={() => window.location.href = props.clicklink} />
-            {isHovered ?
-                (<p className="portrait-learn-more">Click to learn more</p>) :
-                (<p className="portrait-learn-more"><br /></p>)
-            }
+        <div className="potrait-images-container">
+
+            <img
+                ref={refs.setReference}
+                {...getReferenceProps()}
+                src={props.src}
+                alt={props.alt}
+            />
+
             <span className="potrait-text-container">
                 <h3>{props.name}</h3>
                 <h3>{props.subtitle}</h3>
             </span>
+
+            {isHovered &&
+                (<div onClick={() => window.location.href = props.clicklink}
+                    className="portrait-learn-more"
+                    ref={refs.setFloating}
+                    style={floatingStyles}
+                    {...getFloatingProps()}>
+                    <FloatingArrow ref={arrowRef} context={context} />
+                    LinkedIn
+                </div>)
+            }
+
         </div>
     );
 }
--- src/components/about-us/PortraitCollection.tsx
+++ src/components/about-us/PortraitCollection.tsx
@@ -1,16 +1,24 @@
 import Picture from "./Picture";
 
+/* interface portraitData {
+    src: string;
+    alt: string;
+    name: string;
+    subtitle: string;
+    clicklink: string;
+} */
+
 function PortraitCollection() {
     return (
         <>
             <div className="align-center">
                 <div className="potrait-divs-container">
                     <Picture src="/not-not-blank.png" alt="Portrait 1" name="guy" subtitle="guy guy" clicklink="https://google.com" />
-                    <Picture src="/not-not-blank.png" alt="Portrait 1" name="guy" subtitle="guy guy" clicklink="https://google.com" />
-                    <Picture src="/not-not-blank.png" alt="Portrait 1" name="guy" subtitle="super long gay guy loving guy" clicklink="https://google.com" />
-                    <Picture src="/not-not-blank.png" alt="Portrait 1" name="guy" subtitle="guy guy" clicklink="https://google.com" />
-                    <Picture src="/not-not-blank.png" alt="Portrait 1" name="not guy" subtitle="women amirite chat" clicklink="https://google.com" />
-                    <Picture src="/not-not-blank.png" alt="Portrait 1" name="guy" subtitle="guy guy" clicklink="https://google.com" />
+                    <Picture src="/not-not-blank.png" alt="Portrait 2" name="a guy" subtitle="not the guy" clicklink="https://google.com" />
+                    <Picture src="/not-not-blank.png" alt="Portrait 3" name="gay guy" subtitle="super long gay guy loving guy" clicklink="https://google.com" />
+                    <Picture src="/not-not-blank.png" alt="Portrait 4" name="the guy" subtitle="not just a guy" clicklink="https://google.com" />
+                    <Picture src="/not-not-blank.png" alt="Portrait 5" name="not guy" subtitle="women amirite chat" clicklink="https://google.com" />
+                    <Picture src="/not-not-blank.png" alt="Portrait 6" name="look a guy!" subtitle="surpsrise surprise" clicklink="https://google.com" />
                 </div>
             </div>
         </>
