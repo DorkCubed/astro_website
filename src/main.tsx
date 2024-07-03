b17a90e609da9990bbb7f1828ee5853de6ffb268
DorkCubed <f20231087@pilani.bits-pilani.ac.in>
2024-07-03 16:30:40 +0530

    remove unused dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AboutUs from './AboutUs.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
