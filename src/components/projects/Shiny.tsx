3bc16f1c6e053715cc4ac55477384ff54c4f279a
DorkCubed <f20231087@pilani.bits-pilani.ac.in>
2024-07-07 14:09:13 +0530

    projects site updated

--- src/components/projects/Shiny.tsx
+++ src/components/projects/Shiny.tsx
@@ -1,39 +1,45 @@
-import { useState } from "react";
-import { useInteractions, useClientPoint, useFloating, useHover, offset } from "@floating-ui/react";
-import shine from "/shine.png";
+import { useState, useEffect } from "react";
 
 interface ShinyProps {
     Name: string;
     Class: string;
+    Image: string;
 }
 
 function Shiny(props: ShinyProps) {
-    const [isOpen, setIsOpen] = useState(false);
+    const [cursorX, setCursorX] = useState(0);
+    const [cursorY, setCursorY] = useState(0);
 
-    const { refs, floatingStyles, context } = useFloating({
-        middleware: [offset({ mainAxis: -60 })],
-        open: isOpen,
-        onOpenChange: setIsOpen,
-    });
+    useEffect(() => {
+        const card = document.querySelector('.card-container');
 
-    const clientPoint = useClientPoint(context);
-    const hover = useHover(context);
+        if (card) {
+            const handleMouseMove = (event: MouseEvent) => {
+                setCursorX(event.clientX - card.getBoundingClientRect().left);
+                setCursorY(event.clientY - card.getBoundingClientRect().top);
+            };
 
-    const { getReferenceProps, getFloatingProps } = useInteractions([hover, clientPoint]);
+            document.addEventListener("mousemove", handleMouseMove);
+
+            return () => document.removeEventListener("mousemove", handleMouseMove);
+        }
+    }, []);
 
     return (
-        <div className={`item ${props.Class}`}
-            ref={refs.setReference} {...getReferenceProps()}>
-            <div className="shine-bordered" />
-            {isOpen &&
-                <div className="shine-container"
-                    ref={refs.setFloating}
-                    {...getFloatingProps()}
-                    style={floatingStyles}>
-                    <img src={shine} />
-                </div>}
-            <p> {props.Name} </p>
-        </div>
+        <div className={`card-wrapper ${props.Class}`} >
+            <div className='card-container' />
+            < div className="card" style={{
+                position: 'absolute',
+                top: cursorY,
+                left: cursorX,
+                pointerEvents: 'none',
+                transition: '0.5s, top 0s, left 0s',
+            }} />
+            <div className="card-overlay">
+                <img src={props.Image} alt={props.Name} />
+                <p>{props.Name}</p>
+            </div>
+        </div >
     );
 }
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,28 @@
 import "./projects.css";
 import Nav from "../components/Nav";
 import Shiny from "../components/projects/NewShiny";
+import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 function Projects() {
+    const placeholder = "/not-not-blank.png";
+
+    useEffect(() => {
+        const cards = document.querySelectorAll('.card-overlay') as NodeListOf<HTMLElement>;
+
+        cards.forEach((card) => {
+            function handleWindowResize() {
+                card.style.backgroundPositionX = `-${card.getBoundingClientRect().left}px`;
+                card.style.backgroundPositionY = `-${card.getBoundingClientRect().top}px`;
+            }
+
+            handleWindowResize();
+            window.addEventListener("resize", handleWindowResize);
+
+            return () => window.removeEventListener("resize", handleWindowResize);
+        })
+    }, [])
+
     return (
         <>
             <Helmet>
@@ -13,10 +32,10 @@ function Projects() {
             <div className="projects">
                 <Nav />
                 <div className="title-container" >
-                    <Shiny Name="Projects" Class="a" />
-                    <Shiny Name="Projects" Class="b" />
-                    <Shiny Name="Projects" Class="c" />
-                    <Shiny Name="Projects" Class="d" />
+                    <Shiny Name="Projects" Class="a" Image={placeholder} />
+                    <Shiny Name="Projects" Class="b" Image={placeholder} />
+                    <Shiny Name="Projects" Class="c" Image={placeholder} />
+                    <Shiny Name="Projects" Class="d" Image={placeholder} />
                 </div>
             </div>
         </>
